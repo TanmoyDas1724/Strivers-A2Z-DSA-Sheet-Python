bebf4608207cf49c147bf5b2fdf2f3a8c302cdc1
Tanmoy Das <tanmoydas.haldia2021@gmail.com>
2026-06-29 11:48:24 +0530

    updated Union of two Sorted array

--- 01.Arrays/1.Easy/07.union_of_two_sorted_array.py
+++ 01.Arrays/1.Easy/07.union_of_two_sorted_array.py
@@ -1,27 +1,40 @@
-def union(arr1,arr2):
-    ans=[]
-    i=0
-    j=0
-    while i<len(arr1) and j<len(arr2):
-        if arr1[i]<arr2[j]:
-            ans.append(arr1[i])
-            i+=1
-        elif arr1[i]>arr2[j]:
-            ans.append(arr2[j])
-            j+=1
+def union(arr1, arr2):
+    ans = []
+    i = 0
+    j = 0
+
+    while i < len(arr1) and j < len(arr2):
+
+        if arr1[i] < arr2[j]:
+            if arr1[i] not in ans:
+                ans.append(arr1[i])
+            i += 1
+
+        elif arr1[i] > arr2[j]:
+            if arr2[j] not in ans:
+                ans.append(arr2[j])
+            j += 1
+
         else:
+            if arr1[i] not in ans:
+                ans.append(arr1[i])
+            i += 1
+            j += 1
+
+    while i < len(arr1):
+        if arr1[i] not in ans:
             ans.append(arr1[i])
-            i+=1
-            j+=1
-    # Append any remaining elements from either array
-    while i<len(arr1):
-        ans.append(arr1[i])
-        i+=1
-    while j<len(arr2):
-        ans.append(arr2[j])
-        j+=1
+        i += 1
+
+    while j < len(arr2):
+        if arr2[j] not in ans:
+            ans.append(arr2[j])
+        j += 1
+
     return ans
 
-arr1=[2,4,6,8,10]
-arr2=[1,2,3,4,5]
-print("The union of two sorted arrays is :",union(arr1,arr2))
+
+arr1 = [1,1,1,1,1,1,1,2,2]
+arr2 = [1,2,3,4,5]
+
+print("The union of two sorted arrays is :", union(arr1, arr2))
