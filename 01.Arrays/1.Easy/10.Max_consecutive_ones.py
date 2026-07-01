c5f71c3b6f85da16201a73879bae6ff0cb500196
Tanmoy Das <tanmoydas.haldia2021@gmail.com>
2026-07-01 19:08:47 +0530

    Max_consecutive_ones

---
+++ 01.Arrays/1.Easy/10.Max_consecutive_ones.py
@@ -0,0 +1,14 @@
+def Max_consecutive_ones(arr):
+    max_count=0
+    count=0
+    for i in range(len(arr)):
+        if arr[i]==1:
+            count+=1
+            max_count=max(max_count,count)
+        else:
+            count=0
+    return max_count
+
+arr=[1, 1, 0, 1, 1, 1, 0, 1, 1]
+
+print("Maximum consecutive ones:", Max_consecutive_ones(arr))
